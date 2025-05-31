22f4da5b7ac42e6d1eaa9d881d8cd9b40e8d65d3
datmt <dat.maithanh19@gmail.com>
2025-05-31 12:35:26 +0700

    login with google

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -7,4 +7,14 @@
 		<!-- <LayoutFooter/> -->
 	</div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { getProfile } = useAuthStore()
+const { user, isLoggedIn } = storeToRefs(useAuthStore())
+const getDataUser = async () => {
+  const resp = await getProfile()
+  user.value = resp?.data
+}
+if (!user.value) {
+  await getDataUser()
+}
+</script>
